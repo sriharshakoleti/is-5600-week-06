ebcfec772b48013f39ac6a67094d8e4e7a4c9320
sriharshakoleti <sriharsha.koleti@slu.edu>
2024-12-01 22:38:27 +0000

    updated cardlist.jsx and app.jsx files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { Route, Routes} from 'react-router-dom';
+import SingleView from './components/SingleView'
+import Header from './components/Header'
+import CardList from './components/CardList'
 import productData from './data/full-products';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello There.</h1>
+       <Header/> 
+      <CardList data = {productData}/>
+
+      <Routes>
+          {/* <Route path="/" element={<CardList data={productData} />} /> */}
+
+          <Route path="/product/:id" element={<SingleView data={productData} />} />
+      </Routes>
+
     </div>
   );
 }
--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -1,5 +1,72 @@
-const CardList = () => {
-  return ();
+import React, {useState} from "react"
+
+import Card from "./Card";
+import Search  from  "./Search"
+import Button from "./Button"
+
+const CardList = ({ data }) => {
+
+
+  const limit = 10;
+  const [offset, setOffset] = useState(0)
+  //const [products, setProducts] = useState(data.slice(0,limit))
+  const [products, setProducts] = useState(data)
+
+  const handlePrevious = () => {
+    setOffset(offset - 10)
+  }
+
+  const handleNext = () => {
+    setOffset(offset + 10)
+  }
+
+
+  const getPaginatedProducts = () => {
+    return products.slice(offset, offset + limit)
+  }
+
+  const filterTags = (tag) => {
+    const filtered = data.filter(product => {
+      if (!tag) {
+        return product
+      }
+       return product.tags.find(({title}) =>title ===tag)
+    })
+
+    setOffset(0)
+    setProducts(filtered)
+  }
+
+
+  return (
+    <div className="cf pa2">
+
+
+      {/* <Search /> */}
+      <Search handleSearch={filterTags}/>
+
+
+
+      <div className="mt2 mb2">
+        {/* {products.map((product) => ( 
+      <Card key={product.id} {...product} /> */}
+        {getPaginatedProducts().map((product) => ( 
+        <Card key={product.id} {...product} />
+
+
+        ))}
+      </div>
+      <div className ="flex items-center justify-center pa4">
+        <Button text = "Previous" handleClick={handlePrevious} />
+        <Button text = "Next" handleClick={handleNext} />  
+      </div>
+
+
+    </div>
+
+
+
+  );
 }
 
 export default CardList;
